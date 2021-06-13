dec9cb142a00997e625d9125040fc32dbad7925e
Shayani Das <shayanidas773@gmail.com>
2021-06-13 20:15:05 +0530

    Create string-stat.c

---
+++ string-stat.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+ 
+int main()
+{
+  	char str[100];
+  	int i, alphabets, digits, special, words;
+  	i = alphabets = digits = words = special = 0;
+ 
+  	printf("\n Please Enter any String  :  ");
+  	gets(str);
+  	
+  	while (str[i] != '\0')
+  	{
+  		if( (str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z') )
+  		{
+  			alphabets++;  	
+ 		}
+  		else if (str[i] >= '0' && str[i] <= '9')
+  		{
+  			digits++;  	
+  		}
+		else if (str[i] == ' ' && str[i+1] != ' ')
+		  {
+		  	words++;
+		  }
+		else
+    		special++;
+    	i++;
+	}
+    printf("\n Number of Alphabets in this String = %d", alphabets);  
+	printf("\n Number of Digits in this String = %d", digits);  
+	printf("\n Number of words in this String = %d", words);  
+	printf("\n Number of Special Characters in this String = %d", special);  	
+  	return 0;
+}
